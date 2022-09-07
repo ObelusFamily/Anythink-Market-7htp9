a3db331dbed01ddddb9a950129dad130b9864bc0
Prasoon Mahawar <prasoonmahawar21@gmail.com>
2022-09-07 16:26:40 +0530

    resolved request

--- frontend/src/agent.js
+++ frontend/src/agent.js
@@ -5,7 +5,7 @@ const superagent = superagentPromise(_superagent, global.Promise);
 
 const BACKEND_URL =
   process.env.NODE_ENV !== "production"
-    ? "http://localhost:3000"
+    ? "http://localhost:3001"
     : process.env.REACT_APP_BACKEND_URL;
 
 const API_ROOT = `${BACKEND_URL}/api`;
